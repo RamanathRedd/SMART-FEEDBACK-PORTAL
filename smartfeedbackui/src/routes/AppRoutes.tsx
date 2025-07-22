5bffb7f0222983b2b71ae136f3d3c61bf583842f
RamanathRedd <ramanathreddyg04@gmail.com>
2025-07-22 12:24:54 +0000

    added token in dashboard

--- smartfeedbackui/src/routes/AppRoutes.tsx
+++ smartfeedbackui/src/routes/AppRoutes.tsx
@@ -1,16 +1,25 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Login from "../pages/Login/Login";
 import Register from "../pages/Register/Register";
 import Dashboard from "../pages/Dashboard/Dashboard";
 
 const AppRoutes: React.FC = () => {
+  const [token, setToken] = useState<string | null>(null);
+  useEffect(() => {
+    setToken(localStorage.getItem("jwtToken"));
+  }, []);
+
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/home" element={<Dashboard />} />
+      <Route
+        path="/home"
+        element={<Dashboard />}
+        // element={token ? <Dashboard /> : <Navigate to="/login" replace />}
+      />
       <Route path="*" element={<Login />} />
     </Routes>
   );
